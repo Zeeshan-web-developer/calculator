2fe5dccc7b3ab9589f1a9776c70f7fc87319a50f
zeeshan ahamd <zeeshan@craxxino.com>
2022-03-05 13:40:32 +0530

    Updates

---
+++ precache-manifest.c69b817fef370b71ba808c9f635f5208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c10a838c482f97194963a53f2c1e345f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f116c1d339176c1b3bfb",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "1ff2a70c968b18c0d8d5",
+    "url": "./static/js/2.2019fdfe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.2019fdfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f116c1d339176c1b3bfb",
+    "url": "./static/js/main.c476922a.chunk.js"
+  },
+  {
+    "revision": "6068826888d4e6e288ed",
+    "url": "./static/js/runtime-main.7ececd10.js"
+  }
+]);
